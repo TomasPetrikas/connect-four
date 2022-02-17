3f6080d12fb08d38bc177cbba67360780672283e
Tomas Petrikas <tomas.petrikas99@gmail.com>
2022-02-17 12:50:59 +0200

    Add initialize and print to board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,35 @@
 # frozen_string_literal: true
 
+require_relative 'color'
+
+# Game board
 class Board
-  # TODO
+  PIECES = ['●'.yellow.bold, '●'.blue.bold].freeze
+  COLS = 7
+  ROWS = 6
+
+  attr_reader :board
+
+  def initialize
+    @board = Array.new(ROWS) { Array.new(COLS) }
+    @board.map! do |row|
+      row.map! do
+        '_'
+      end
+    end
+
+    # @board.last[0] = PIECES[0]
+    # @board.last[2] = PIECES[1]
+  end
+
+  def print_board
+    @board.each do |row|
+      row.each do |cell|
+        print "#{cell} "
+      end
+      puts ''
+    end
+
+    # p @board
+  end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,1 +1,6 @@
 # frozen_string_literal: true
+
+require_relative 'board'
+
+b = Board.new
+b.print_board
